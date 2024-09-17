fe3e986c437eba1c57678ca1044f63cc7989db98
Arm1700 <bukbok1@gmail.com>
2024-09-17 14:40:28 +0400

    allBack4

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'popular_courses',
                 views.PopularCourseViewSet, basename='popularcourse')
 router.register(r'lesson_info', views.LessonInfoViewSet, basename='lessoninfo')
+router.register(r'reviews', ReviewViewSet, basename='review')
 router.register(r'events', views.EventViewSet, basename='event')
 urlpatterns = [
     path('', include(router.urls)),
